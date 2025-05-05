2b1e2a5cf36157234dfca8dfe38f452aeb777314
Bryan Segovia <bryan.segovia@tecsup.edu.pe>
2025-05-05 09:25:33 -0500

    Reder no god

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const http = require('http');
 
-function requestController() {
+function requestController(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bienvenidos al curso\n');
 }
 
 console.log('Bienvenidos al curso');
 
 const server = http.createServer(requestController);
 
-const PORT = process.env.PORT;
+// Usar el puerto proporcionado por Render o el valor de .env
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, function() {
+  console.log("Aplicación corriendo en: " + PORT);
 });
-
-console.log("Aplicación corriendo en: " + PORT);
